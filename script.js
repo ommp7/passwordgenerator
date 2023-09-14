8bad7d14e4d5ca36f45a056f35272b04ea4724e9
Om Patel <ompatel@oms-macbook-air.mynetworksettings.com>
2023-09-14 17:41:25 -0400

    passwordgenerator

---
+++ script.js
@@ -0,0 +1,27 @@
+// Function to generate a random password
+function generatePassword() {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  var passwordLength = 12; // Change the desired password length here
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Get references to the #generate element and password input field
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
